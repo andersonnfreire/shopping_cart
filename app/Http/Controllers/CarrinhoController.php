e25e3c654527260f620325f542a8e48e47f57eab
Anderson N Freire <coolerinformatica1995@gmail.com>
2019-12-18 15:30:31 -0200

    Add code to Pedido and Produto

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Model\Pedido;
 
 class CarrinhoController extends Controller
 {
-    //
+    function __construct()
+    {
+        // obriga estar logado;
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+
+        $pedidos = Pedido::where([
+            'status'  => 'RE',
+            'user_id' => Auth::id()
+            ])->get();
+
+        return view('carrinho.index', compact('pedidos'));
+    }
 }
