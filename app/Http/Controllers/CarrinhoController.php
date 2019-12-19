47fa9b0e78931f03636280cee35326942ae60d61
Anderson N Freire <coolerinformatica1995@gmail.com>
2019-12-19 16:04:30 -0200

    Adicionando métodos de remover e concluir compra

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Model\Pedido;
+use App\Model\PedidoProduto;
+use App\Model\Produto;
+use App\Model\CupomDesconto;
 
 class CarrinhoController extends Controller
 {
@@ -64,6 +67,74 @@ class CarrinhoController extends Controller
         $req->session()->flash('mensagem-sucesso', 'Produto adicionado ao carrinho com sucesso!');
 
         return redirect()->route('carrinho.index');
+
+    }
+    public function remover()
+    {
+
+        $this->middleware('VerifyCsrfToken');
+
+        $req = Request();
+        $idpedido           = $req->input('pedido_id');
+        $idproduto          = $req->input('produto_id');
+        $remove_apenas_item = (boolean)$req->input('item');
+        $idusuario          = Auth::id();
+
+        $idpedido = Pedido::consultaId([
+            'id'      => $idpedido,
+            'user_id' => $idusuario,
+            'status'  => 'RE' // Reservada
+            ]);
+
+        if( empty($idpedido) ) {
+            $req->session()->flash('mensagem-falha', 'Pedido não encontrado!');
+            return redirect()->route('carrinho.index');
+        }
+
+        $where_produto = [
+            'pedido_id'  => $idpedido,
+            'produto_id' => $idproduto
+        ];
+
+        $produto = PedidoProduto::where($where_produto)->orderBy('id', 'desc')->first();
+        if( empty($produto->id) ) {
+            $req->session()->flash('mensagem-falha', 'Produto não encontrado no carrinho!');
+            return redirect()->route('carrinho.index');
+        }
+
+        if( $remove_apenas_item ) {
+            $where_produto['id'] = $produto->id;
+        }
+        PedidoProduto::where($where_produto)->delete();
+
+        $check_pedido = PedidoProduto::where([
+            'pedido_id' => $produto->pedido_id
+            ])->exists();
+
+        if( !$check_pedido ) {
+            Pedido::where([
+                'id' => $produto->pedido_id
+                ])->delete();
+        }
+
+        $req->session()->flash('mensagem-sucesso', 'Produto removido do carrinho com sucesso!');
+
+        return redirect()->route('carrinho.index');
+    }
+    public function compras()
+    {
+
+        $compras = Pedido::where([
+            'status'  => 'PA',
+            'user_id' => Auth::id()
+            ])->orderBy('created_at', 'desc')->get();
+
+        $cancelados = Pedido::where([
+            'status'  => 'CA',
+            'user_id' => Auth::id()
+            ])->orderBy('updated_at', 'desc')->get();
+
+        return view('carrinho.compras', compact('compras', 'cancelados'));
 
     }
 }
